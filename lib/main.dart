8a502e73024a500272bfd71df6bff250cbb2f5af
Alberto Bonacina <bonacina.alberto@gmail.com>
2023-07-19 23:17:38 +0200

    feat: refactor main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:myreadings/app/app.dart';
+import 'package:go_router/go_router.dart';
+import 'package:myreadings/pages/home/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,5 +10,39 @@ void main() async {
     ProviderScope(
       child: App(),
     ),
+  );
+}
+
+class App extends ConsumerWidget {
+  App({Key? key}) : super(key: key);
+
+  late final router = buildRouter();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routeInformationProvider: router.routeInformationProvider,
+      routeInformationParser: router.routeInformationParser,
+      routerDelegate: router.routerDelegate,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.amber,
+          brightness: Brightness.dark,
+        ),
+      ),
+    );
+  }
+}
+
+GoRouter buildRouter() {
+  return GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const HomePage(),
+      ),
+    ],
   );
 }
--- lib/repository/notion_repository.dart
+++ lib/repository/notion_repository.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
-import 'dart:io';
 
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:myreadings/models/notion_page.dart';
 
@@ -10,21 +8,11 @@ class NotionRepository {
 
   Future<List<NotionPage>?> getBookList() async {
     var response = await http.post(
-      Uri.parse(
-          '${const String.fromEnvironment('NOTION_BASE_URL')}/${const String.fromEnvironment('NOTION_DB_BOOKS')}/query'),
-      headers: {
-        HttpHeaders.contentTypeHeader: 'application/json',
-        HttpHeaders.authorizationHeader:
-            'Bearer ${const String.fromEnvironment('NOTION_SECRET_KEY')}',
-        'Notion-Version': const String.fromEnvironment('NOTION_API_VERSION')
-      },
+      Uri.parse(const String.fromEnvironment('WORKER_URL')),
       body: '{"page_size": 100}',
     );
-    debugPrint(response.toString());
-    debugPrint(response.body.toString());
 
     final responseBodyParsed = json.decode(response.body);
-    debugPrint(responseBodyParsed.toString());
     return responseBodyParsed["results"]
         .map<NotionPage>((json) => NotionPage.fromJson(json))
         .toList();
