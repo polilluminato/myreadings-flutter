39d6b1642e6e293312511d854ce9c616aee0732f
Alberto Bonacina <bonacina.alberto@gmail.com>
2023-07-20 23:21:52 +0200

    fix: Remove # in the url

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:go_router/go_router.dart';
 import 'package:myreadings/pages/home/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  usePathUrlStrategy();
 
   runApp(
     ProviderScope(
