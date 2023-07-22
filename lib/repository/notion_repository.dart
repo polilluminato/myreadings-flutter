b7674c914e23a6fc63391a978ac9f1e338337ff0
Alberto Bonacina <bonacina.alberto@gmail.com>
2023-07-22 11:15:18 +0200

    feat: add sorting while calling Notion

--- lib/repository/notion_repository.dart
+++ lib/repository/notion_repository.dart
@@ -9,7 +9,12 @@ class NotionRepository {
   Future<List<NotionPage>?> getBookList() async {
     var response = await http.post(
       Uri.parse(const String.fromEnvironment('WORKER_URL')),
-      body: '{"page_size": 100}',
+      body: '{"page_size":100,'
+          '"sorts":['
+          '{"property":"Finished","direction":"ascending"},'
+          '{"property":"Progress","direction":"descending"},'
+          '{"property":"Date","direction":"descending"}'
+          ']}',
     );
 
     Map<String, dynamic> responseBodyParsed = json.decode(response.body);
