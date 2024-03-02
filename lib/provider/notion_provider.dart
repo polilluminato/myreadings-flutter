8a4b68cd547ebe883dd76df66b7322d83b3e14e4
Alberto Bonacina <bonacina.alberto@gmail.com>
2024-03-02 11:56:32 +0100

    feat: Add right repository

--- lib/provider/notion_provider.dart
+++ lib/provider/notion_provider.dart
@@ -1,11 +1,11 @@
 import 'package:myreadings/models/notion_page.dart';
-import 'package:myreadings/repository/notion_fake_repository.dart';
+import 'package:myreadings/repository/notion_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'notion_provider.g.dart';
 
 @riverpod
 Future<List<NotionPage>?> bookList(BookListRef ref) async {
-  return NotionFakeRepository().getBookList();
-  //return NotionRepository().getBookList();
+  //return NotionFakeRepository().getBookList();
+  return NotionRepository().getBookList();
 }
